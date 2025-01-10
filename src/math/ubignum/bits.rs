1cbbe4522ad291b92270649fb0952f59bf4b4d08
Frondorf, Niklas <niklas@frondorf.eu>
2025-01-10 11:22:27 +0100

    feat(ubignum): added missing bit operations

--- src/math/ubignum/bits.rs
+++ src/math/ubignum/bits.rs
@@ -13,6 +13,7 @@ macro_rules! check_pos {
 }
 
 impl<const NUM_DIGITS: usize> UBignum<NUM_DIGITS> {
+    #[inline]
     pub fn get_bit(&self, pos: usize) -> bool {
         check_pos!(NUM_DIGITS, pos);
 
@@ -21,6 +22,7 @@ impl<const NUM_DIGITS: usize> UBignum<NUM_DIGITS> {
         (chunk >> (pos % 64)) & 1 == 1
     }
 
+    #[inline]
     pub fn set_bit(&mut self, pos: usize) {
         check_pos!(NUM_DIGITS, pos);
 
@@ -32,6 +34,7 @@ impl<const NUM_DIGITS: usize> UBignum<NUM_DIGITS> {
         }
     }
 
+    #[inline]
     pub fn unset_bit(&mut self, pos: usize) {
         check_pos!(NUM_DIGITS, pos);
 
@@ -46,6 +49,29 @@ impl<const NUM_DIGITS: usize> UBignum<NUM_DIGITS> {
             if *e != 0 || i == 0 {
                 self.pos = i;
                 return;
+            }
+        }
+    }
+
+    #[inline]
+    pub fn toggle_bit(&mut self, pos: usize) {
+        check_pos!(NUM_DIGITS, pos);
+
+        let chunk_pos = pos / 64;
+        self.digits[chunk_pos] ^= 1 << (pos % 64);
+
+        if self.digits[chunk_pos] != 0 {
+            // got bigger
+            if chunk_pos > self.pos {
+                self.pos = chunk_pos;
+            }
+        } else {
+            // got smaller
+            for (i, e) in self.digits[0..self.len()].iter().enumerate().rev() {
+                if *e != 0 || i == 0 {
+                    self.pos = i;
+                    return;
+                }
             }
         }
     }
@@ -104,6 +130,32 @@ mod tests {
             let mut bn1: UBignum<3> = bn.clone();
             bn1.unset_bit(i);
             let bn2: UBignum<3> = py_test(&format!("{s} & ~(1 << {i})"));
+
+            assert_eq!(bn1, bn2);
+        }
+    }
+
+    #[test]
+    fn toggle_bit1() {
+        let s = "0x0102030405060708090a0b0c0d0e0fabcdef0";
+        let bn: UBignum<3> = UBignum::try_from_hex_string(s).unwrap();
+        for i in 0..64 * 3 {
+            let mut bn1: UBignum<3> = bn.clone();
+            bn1.toggle_bit(i);
+            let bn2: UBignum<3> = py_test(&format!("{s} ^ (1 << {i})"));
+
+            assert_eq!(bn1, bn2);
+        }
+    }
+
+    #[test]
+    fn toggle_bit2() {
+        let s = "0x0100000000000100000000000000010000000";
+        let bn: UBignum<3> = UBignum::try_from_hex_string(s).unwrap();
+        for i in 0..64 * 3 {
+            let mut bn1: UBignum<3> = bn.clone();
+            bn1.toggle_bit(i);
+            let bn2: UBignum<3> = py_test(&format!("{s} ^ (1 << {i})"));
 
             assert_eq!(bn1, bn2);
         }
